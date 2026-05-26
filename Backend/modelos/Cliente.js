849d1a0fd6c988e185f09fa2cc16e35b2368682e
pipe720 <felipevalen7@gmail.com>
2026-05-26 03:15:11 -0400

    Primeros modelos BD

--- Backend/modelos/Cliente.js
+++ Backend/modelos/Cliente.js
@@ -1,14 +1,24 @@
 const { Schema, model } = require("mongoose");
 
-
-
-
-
-
-
-
-
-
+const ClienteSchema = Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    correo: {
+        type: String,
+        required: true
+    },
+    contrasena: {
+        type: String,
+        required: true,
+        trim: true //Elimina espacios blancos al inicio o final
+    },
+    rut: {
+        type: String,
+        required: true
+    }
+});
 
 
 module.exports = model("Cliente", ArticuloSchema, "clientes");
--- Backend/modelos/Pedido.js
+++ Backend/modelos/Pedido.js
@@ -1,8 +1,47 @@
 const { Schema, model } = require("mongoose");
 
+const PedidoSchema = Schema({
+    idpedido: {
+        type: String,
+        required: true
+    },
+    fecha: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    estado: {
+        type: String,
+        required: true
+    },
+    cliente: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'Cliente',
+        required: true
+    }, //Para referenciar al cliente, se puede extraer todos los datos de ahi.
 
+    producto: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'Producto',
+        required: true
+    }, //Para referenciar al producto, se puede extraer todos los datos de ahi.
+    
+    total: {
+        type: Number,
+        required: true
+    },
 
+    descripcion: {
+        type: String,
+    },
 
+    direccion: {
+        type: String,
+        required:true
+    },
 
+    telefono: {
+        type: String,
+        required:true
+    }
+});
 
 module.exports = model("Pedido", ArticuloSchema, "pedidos");
