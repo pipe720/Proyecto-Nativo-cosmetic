d52afafcca3e0376507e87b061582a1371cff973
pipe720 <felipevalen7@gmail.com>
2026-06-01 04:06:36 -0400

    probando

--- nativocosmetic/src/App.js
+++ nativocosmetic/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { MiContextoProvider } from "./MiContexto";
+import Home from './Home';
 
 function App() {                    
-  return (                                              /*AQUI SE VAN A CARGAS LAS RUTAS con react-router-do */
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (   
+    <MiContextoProvider>
+    <Router>
+      <Routes>         
+         <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </MiContextoProvider>  
+    /*AQUI SE VAN A CARGAS LAS RUTAS con react-router-do */
   );
 }
 
--- nativocosmetic/tailwind.config.js
+++ nativocosmetic/tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // tus componentes React
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+});
