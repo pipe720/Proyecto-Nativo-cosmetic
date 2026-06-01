5addbc4cc286ba08e0ccfbea59639983a5c70f7f
pipe720 <felipevalen7@gmail.com>
2026-06-01 04:17:24 -0400

    Prueba Taildwind en Home.js y creacion rutas en App.js

---
+++ nativocosmetic/src/Home.js
@@ -0,0 +1,23 @@
+import {
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Button,
+} from "@material-tailwind/react";
+ 
+function Home() {
+  return (
+    <Menu>
+      <MenuHandler>
+        <Button>Menu</Button>
+      </MenuHandler>
+      <MenuList>
+        <MenuItem>Menu Item 1</MenuItem>
+        <MenuItem>Menu Item 2</MenuItem>
+        <MenuItem>Menu Item 3</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
+export default Home;
